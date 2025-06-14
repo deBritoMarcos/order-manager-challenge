79ecf77791e870b211ab912e1286aaf40fa11431
Marcos Brito <marcosbrito.dev@gmail.com>
2025-06-13 22:26:53 -0300

    test: fix incorret mock in ControlOrderProgressServiceTest

--- backend/tests/Unit/Service/Order/ControlOrderProgressServiceTest.php
+++ backend/tests/Unit/Service/Order/ControlOrderProgressServiceTest.php
@@ -2,12 +2,9 @@
 
 use App\Data\Order\OrderData;
 use App\Enum\Order\OrderStatus;
+use App\Models\Order;
 use App\Repository\Order\Contracts\OrderEloquentRepositoryInterface;
 use App\Service\Order\Contracts\ControlOrderProgressServiceInterface;
-
-beforeEach(function () {
-    $this->service = app()->make(ControlOrderProgressServiceInterface::class);
-});
 
 it('must have a contract', function () {
     expect(app()->bound(OrderEloquentRepositoryInterface::class))
@@ -22,13 +19,20 @@ test('`register` creates order with pending status', function () {
 
     $this->mock(OrderEloquentRepositoryInterface::class)
         ->shouldReceive('create')
+        ->once()
         ->withArgs(function(OrderData $data) use ($expectedData) {
             return $data->code === $expectedData->code
                 && $data->status === $expectedData->status;
         })
+        ->andReturn(Order::factory()->create([
+            'code' => 123456,
+            'status' => OrderStatus::Pending
+        ]))
         ->getMock();
+    
+    $service = app()->make(ControlOrderProgressServiceInterface::class);
 
-    $orderCreated = $this->service
+    $orderCreated = $service
         ->register(123456);
 
     expect($orderCreated)
